88abb5d40511c8636db887f12bbc866fab59e892
= <demysdcl@gmail.com>
2020-04-19 16:18:50 -0300

    init project

--- src/main/java/com/hugosilva/curso/ws/security/WebConfigCorsRegistery.java
+++ src/main/java/com/hugosilva/curso/ws/security/WebConfigCorsRegistery.java
@@ -3,6 +3,7 @@ package com.hugosilva.curso.ws.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,5 +18,14 @@ public class WebConfigCorsRegistery  implements WebMvcConfigurer{
                 .allowedMethods("POST, PUT, GET, OPTIONS, DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(false).maxAge(3600);
+    }
+    /*Este método permite inicializar swagger-ui.html da API REST*/
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
